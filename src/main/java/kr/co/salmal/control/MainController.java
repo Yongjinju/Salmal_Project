1471b46445824a4a96bb41aa57b64a9eabf777ad
jiw-dev <ev.electronvolt@gmail.com>
2019-09-26 20:16:20 +0900

    [수정] MainController.java 사용하지 않는 import문장 제거

--- src/main/java/kr/co/salmal/control/MainController.java
+++ src/main/java/kr/co/salmal/control/MainController.java
@@ -23,7 +23,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
-import kr.co.salmal.domain.MailVO;
 import kr.co.salmal.domain.MemberVO;
 import kr.co.salmal.persistence.ArticleDAO;
 import kr.co.salmal.persistence.MemberDAO;
